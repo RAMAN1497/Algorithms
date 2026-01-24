aa8070c1e0eba2c280b9a8a5117fdaa8c3c5cf8f
RAMAN1497 <ramankumar140907@gmail.com>
2026-01-24 21:45:23 +0530

    tips_from_velocity.cpp

--- Lab_1/tips_from_velocity.cpp
+++ Lab_1/tips_from_velocity.cpp
@@ -9,31 +9,16 @@ int findTips(double v)
     if (v < 1.0)
         return 0;
 
-    return 1 + findTips(v - 0.425);
+    return 1 + findTips(v - 0.425*v);
 }
 
 int main() {
-
-    int repetitions = 100;
-
-    cout << "Input(v)\tAverageTime(us)\n";
-
-    for (double v = 5; v <= 50; v += 5) {
-
-        long long totalTime = 0;
-
-        for (int i = 0; i < repetitions; i++) {
-
-            auto start = high_resolution_clock::now();
-            findTips(v);
-            auto end = high_resolution_clock::now();
-
-            totalTime += duration_cast<microseconds>(end - start).count();
-        }
-
-        double avgTime = (double)totalTime / repetitions;
-        cout << v << "\t\t" << avgTime << endl;
-    }
+int v;
+   cout<<"Enter the velocity:";
+   cin>>v;
+          cout<<  findTips(v);
+         
+    
 
     return 0;
 }
