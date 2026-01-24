d050338c95b866844dbb4f9ebc83a7f9120e4867
RAMAN1497 <ramankumar140907@gmail.com>
2026-01-24 21:43:05 +0530

    hornor_rule.cpp

--- Lab_1/hornor_rule.cpp
+++ Lab_1/hornor_rule.cpp
@@ -1,44 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <chrono>
 using namespace std;
-using namespace std::chrono;
 
-// Horner's Rule function
-long long horner(const vector<int>& coeff, int x, int index) {
-    // Base case: last coefficient
-    if (index == coeff.size() - 1)
-        return coeff[index];
-
-    // Recursive call
-    return coeff[index] + x * horner(coeff, x, index + 1);
+long long horner(int coeff[], int n, int x, int i) {
+    if (i == n)
+        return coeff[i];
+    return coeff[i] + x * horner(coeff, n, x, i + 1);
 }
 
 int main() {
-
-    int repetitions = 100;
+    int n = 4;
+    int coeff[] = {1, 2, 3, 4, 5};
     int x = 2;
 
-    cout << "Degree\tAverageTime(us)\n";
-
-    for (int degree = 100; degree <= 1000; degree += 100) {
-
-        vector<int> coeff(degree + 1, 1); // all coefficients = 1
-        long long totalTime = 0;
-
-        for (int i = 0; i < repetitions; i++) {
-
-            auto start = high_resolution_clock::now();
-            horner(coeff, x,0);
-            auto end = high_resolution_clock::now();
-
-            auto duration = duration_cast<microseconds>(end - start);
-            totalTime += duration.count();
-        }
-
-        double avgTime = (double)totalTime / repetitions;
-        cout << degree << "\t" << avgTime << endl;
-    }
+    cout << "Polynomial value: " << horner(coeff, n, x, 0);
 
     return 0;
 }
