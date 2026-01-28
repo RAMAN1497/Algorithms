08710cded4e087895f55ebf1cf736addcc487915
RAMAN1497 <ramankumar140907@gmail.com>
2026-01-28 20:21:13 +0530

    Lab 1 program

--- Lab_1/permutation.cpp
+++ Lab_1/permutation.cpp
@@ -4,35 +4,30 @@
 using namespace std;
 using namespace std::chrono;
 
-// Function to generate permutations
 void permute(string &s, int l, int r) {
     if (l == r) {
-        // Just generate, do not print (printing affects time)
         return;
     }
 
     for (int i = l; i <= r; i++) {
         swap(s[l], s[i]);
         permute(s, l + 1, r);
-        swap(s[l], s[i]); // backtrack
+        swap(s[l], s[i]); 
     }
 }
 
 int main() {
-
-    int repetitions = 100;
-
     cout << "InputSize\tAverageTime(us)\n";
 
     for (int size = 3; size <= 10; size++) {
 
         string s = "";
         for (int i = 0; i < size; i++)
-            s += char('A' + i);   // ABC, ABCD, ...
+            s += char('A' + i);  
 
         long long totalTime = 0;
 
-        for (int i = 0; i < repetitions; i++) {
+        for (int i = 0; i < 100; i++) {
 
             auto start = high_resolution_clock::now();
             permute(s, 0, size - 1);
@@ -48,3 +43,4 @@ int main() {
 
     return 0;
 }
+
